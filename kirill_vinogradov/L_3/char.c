25e9a525f9c6f8e5f392ca57d8b7a4da17647959
zoOm60rus <xam9lk60rus@gmail.com>
2016-11-24 12:56:51 +0300

    lab 3 updated

--- kirill_vinogradov/L_3/char.c
+++ kirill_vinogradov/L_3/char.c
@@ -2,16 +2,19 @@
 #include<stdlib.h>
 #include<string.h>
 
-int odd_sent(char* arr);
+char* DeleitePlz(char* arr);
 
-char* deleite_plz(char* arr);
+int OddSent(char* arr);
 
 int main(){
-   char* a=(char*)malloc(500*sizeof(char)); 
-   char* c=(char*)malloc(500*sizeof(char));
+   char* a;
+   char* c;
+   a=(char*)malloc(1*sizeof(char)); 
+   c=(char*)malloc(1*sizeof(char));
    char b;
     int i=0, j=0, n=0, m=0;
     while((b=getchar())!='!'){
+        a=(char*)realloc(a, (i+3)*sizeof(char));
         a[i++]=b;
       }
     a[i++]='!';
@@ -19,27 +22,28 @@ int main(){
     i=0;
     while(i<strlen(a)){  
         while((a[i]!='.') && (a[i]!='?') && (a[i]!=';') && (a[i]!='!')){
+            c=(char*)realloc(c, (i+3)*sizeof(char));
             c[j++]=a[i];
             i++;
         }
         c[j++]=a[i];
         c[j]='\0';
         j=0;
-        if(odd_sent(c)==0){
-            printf("%s\n", deleite_plz(c));
+        if(OddSent(c)==0){
+            printf("%s\n", DeleitePlz(c));
             m++;
         }
         n++;
         i++;   
       }
-    printf("РљРѕР»РёС‡РµСЃС‚РІРѕ РїСЂРµРґР»РѕР¶РµРЅРёР№ РґРѕ %d Рё РєРѕР»РёС‡РµСЃС‚РІРѕ РїСЂРµРґР»РѕР¶РµРЅРёР№ РїРѕСЃР»Рµ %d\0", n-1, m);
+    printf("Количество предложений до %d и количество предложений после %d\n", n-1, m);
     return 0;
 }
 
-char* deleite_plz(char* arr){
-    int i, j, k;
+char* DeleitePlz(char* arr){
+    int i;
     while((arr[0]=='\t') || (arr[0]==' ') || (arr[0]=='\n')){
-        for(i=0;i<strlen(arr)-1;i++){
+        for(i=0;i<(strlen(arr)-1);i++){
             arr[i]=arr[i+1];
         }
         arr[strlen(arr)-1]='\0';
@@ -47,11 +51,11 @@ char* deleite_plz(char* arr){
     return arr;
 }
   
-int odd_sent(char* arr){
+int OddSent(char* arr){
     int i, j=0;
     for(i=0;i<strlen(arr);i++){
-        if(arr[i]=='?' || arr[i]=='!'){
-            j++;
+        if((arr[i]=='?') || (arr[i]=='!')){
+            j=1;
         }
     }
     return j;
